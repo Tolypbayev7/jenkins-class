2b7afbb1432bc53a90662ca1172932e6ad909531
Tolypbayev7 <126831888+Tolypbayev7@users.noreply.github.com>
2023-06-13 20:24:55 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,27 +1,27 @@
 variable ami_id {
   type        = string
-  default     = “”
-  description = “description”
+  default     = ""
+  description = "description"
 }
 variable instance_type {
   type        = string
-  default     = “”
-  description = “description”
+  default     = ""
+  description = "description"
 }
 variable az1 {
   type        = string
-  default     = “”
-  description = “description”
+  default     = ""
+  description = "description"
 }
 variable key_pair {
   type        = string
-  default     = “”
-  description = “description”
+  default     = ""
+  description = "description"
 }
 variable region {
   type        = string
-  default     = “”
-  description = “description”
+  default     = ""
+  description = "description"
 }
 
 
